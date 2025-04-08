0d10f5882b30dda622321858d50bff86c2b49015
Eric Dallo <ericdallo06@hotmail.com>
2025-04-08 13:11:13 -0300

    Add more data

--- src/metrepl/exporters.clj
+++ src/metrepl/exporters.clj
@@ -6,11 +6,25 @@
   (:import
    [java.time Instant]))
 
+(defn ^:private system-data* []
+  {:os-name (System/getProperty "os.name")
+   :os-version (System/getProperty "os.version")
+   :os-arch (System/getProperty "os.arch")
+   :hostname (try (.getHostName (java.net.InetAddress/getLocalHost))
+                  (catch java.net.UnknownHostException _ nil))})
+
+(def ^:private system-data (memoize system-data*))
+
+(defn ^:private enhance-data [data metric-cfg]
+  (merge
+   (assoc data
+          :timestamp (Instant/now)
+          :level (:level metric-cfg))
+   (system-data)))
+
 (defn ^:private export!* [metric data]
   (let [metric-cfg (config/metric metric)
-        data (assoc data
-                    :timestamp (Instant/now)
-                    :level (:level metric-cfg))]
+        data (enhance-data data metric-cfg)]
     (when-not (identical? :off (:level metric-cfg))
       (doseq [[exporter exporter-cfg] (config/exporters)]
         (when (:enabled? exporter-cfg)
